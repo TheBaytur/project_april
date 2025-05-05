418090ce9dc0bc2e6e607711076a4f8ff68d46cf
TheBaytur <bayturcodding@gmail.com>
2025-05-05 12:30:03 +0800

    AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -7,83 +7,22 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
-          brightness: Brightness.dark,
+          seedColor: Colors.teal,
+      brightness: Brightness.dark,
+      ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter mapp'),
         ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-        leading: Icon(Icons.notification_add, color: Colors.white,),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(50.0),
-        child: Stack(
-          children: [
-            Image.asset('assets/images/bg.jpg',
-            fit:BoxFit.cover,
-              height: 300,
-            ),
-            SizedBox( height: 300,
-              child: Center(child: Text('Kato Sicarius',style:
-              TextStyle(color: Colors.white,fontSize: 30),)),
-            ),
-            ListTile(
-              leading: Icon(Icons.join_full),
-              tileColor: Colors.red,
-              title: Text("Ultramarine's champions"),
-              trailing: Text('Characteristics'),
-              onTap: (){
-
-              },
-            )
-          ],
-        ),
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
