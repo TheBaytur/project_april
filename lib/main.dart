dc7055d6ba8a6c6fbceac8c878e9b668b0facf0c
TheBaytur <bayturcodding@gmail.com>
2025-05-07 15:46:19 +0800

    Home & Profile & Search

--- lib/main.dart
+++ lib/main.dart
@@ -4,65 +4,40 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.teal,
-      brightness: Brightness.dark,
-      ),
-      ),
-      home: MyWidget()
-    );
-  }
-}
-
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
-
-  @override
-  State<MyWidget> createState() => _MyWidgetState();
-}
-
-class _MyWidgetState extends State<MyWidget> {
-
-  int currentIndex = 0;
-  @override
-  Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter mapp'),
-        centerTitle: true,
-      ),
-      body: currentIndex == 0
-          ? Center(
-            child: Text('Home'),
-          )
-          : Center(
-            child: Text('Profile'),
-          ),
-
-      bottomNavigationBar: NavigationBar(destinations: [
-        NavigationDestination(icon: Icon(Icons.home),
-          label: 'Home',
+        seedColor: Colors.deepPurple,
+          brightness: Brightness.dark,
+    ),
+    ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Flutter Mapp'),
         ),
-        NavigationDestination(icon: Icon(Icons.person),
-          label: 'Profile',
-        ),
-      ],
-      onDestinationSelected: (int value) {
-        setState(() {
-          currentIndex = value;
-        });
-      },
-        selectedIndex: currentIndex,
+        bottomNavigationBar: NavigationBar(
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Icons.search), label: 'Search'),
+            NavigationDestination(icon: Icon(Icons.settings), label: 'Settings'),
 
+        ],
+          onDestinationSelected: (int value){
+            selectedIndex = value;
+          },
+          selectedIndex: selectedIndex,
+        ),
       ),
     );
   }
