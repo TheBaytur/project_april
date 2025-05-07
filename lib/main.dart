81128ffc859b7ddd54605fbc466ed41320a24a1d
TheBaytur <bayturcodding@gmail.com>
2025-05-07 22:22:15 +0800

    pages

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:project_april/views/widget_tree.dart';
 import 'package:project_april/views/widgets/navbar_widgets.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
