66d9de3b70803b1985bc1783ffaf1d291a739efc
TheBaytur <bayturcodding@gmail.com>
2025-05-07 21:41:36 +0800

    Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:project_april/widgets/navbar_widgets.dart';
+import 'package:project_april/widgets/navbar_widgets.dart'; //Auto
 
 void main() {
   runApp(const MyApp());
