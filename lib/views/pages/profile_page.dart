a1a694c2d0f636c1e340a745afe2506d5c7fd9b2
TheBaytur <bayturcodding@gmail.com>
2025-05-12 22:05:37 +0800

    Profile page

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -90,7 +90,7 @@ class _ProfilePageState extends State<ProfilePage> {
             InkWell(
               splashColor: Colors.teal,
               onTap: () {
-                print('Image selected'); // Print
+                print('Image selected');
               },
               child: Container(
                 height: 200,
