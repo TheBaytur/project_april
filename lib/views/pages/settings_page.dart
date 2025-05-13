5435ca37c2b83e90f92282003d09d9795dfdb047
TheBaytur <bayturcodding@gmail.com>
2025-05-13 14:37:56 +0800

    Settings pop button

--- lib/views/pages/settings_page.dart
+++ lib/views/pages/settings_page.dart
@@ -19,6 +19,11 @@ class _SettingsPageState extends State<SettingsPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Settings'),
+        leading: BackButton(onPressed: () {
+          Navigator.pop(context);
+        },
+        ),
+        automaticallyImplyLeading: false,
       ),
       body: SingleChildScrollView(
         child: Padding(
