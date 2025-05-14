699adb29a1010bb91f8284d2bceb882c26fbefeb
TheBaytur <bayturcodding@gmail.com>
2025-05-14 15:54:34 +0800

    Style

---
+++ lib/data/constants.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class KTextStyle {
+  static const TextStyle titleTealText = TextStyle(
+    color: Colors.teal,
+    fontSize: 18.0,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle descriptionTealText = TextStyle(
+      fontSize: 16.0);
+}
--- lib/views/pages/home_pages.dart
+++ lib/views/pages/home_pages.dart
@@ -1,31 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:project_april/data/constants.dart';
 import 'package:project_april/views/widgets/hero_widget.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage ({super.key});
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: EdgeInsets.all(20.0),
-        child: Column(
-            children: [
-              HeroWidget(),
-              Container(
-                width: double.infinity,
-                padding: EdgeInsets.symmetric(vertical: 10.0),
-                child: Card(child: Padding(padding: EdgeInsets.all(20.0),
+    return Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          HeroWidget(),
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(vertical: 10.0),
+            child: Card(
+              child: Padding(
+                padding: EdgeInsets.all(20.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('Basic layout'),
-                    Text('The Description of this'),
+                    Text('Basic layout', style: KTextStyle.titleTealText),
+
+                    Text(
+                      'The Description of this',
+                      style: KTextStyle.descriptionTealText,
+                    ),
                   ],
                 ),
-                ),
-                ),
               ),
-              ],
-              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
