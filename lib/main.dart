7b29eccfe1db9ddc4fed5da77781955ba875f83b
TheBaytur <bayturcodding@gmail.com>
2025-05-16 14:24:38 +0800

    Local

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:project_april/data/notifiers.dart';
 import 'package:project_april/views/pages/welcome_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-
 void main() {
   runApp(const MyApp()); //main
 }
@@ -17,32 +16,33 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   initState() {
     initThemeMode();
     super.initState();
   }
 
-  void initThemeMode() async {final SharedPreferences prefs = await SharedPreferences.getInstance();
+  void initThemeMode() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
     final bool? repeat = prefs.getBool(KConstants.themeModeKey);
     isDarkMode.value = repeat ?? false;
   }
 
   Widget build(BuildContext context) {
-    return ValueListenableBuilder(valueListenable: isDarkMode,
-        builder: (context, isDarkMode, child) {
-      return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.deepPurple,
-            brightness: isDarkMode ? Brightness.dark : Brightness.light,
+    return ValueListenableBuilder(
+      valueListenable: isDarkMode,
+      builder: (context, isDarkMode, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.deepPurple,
+              brightness: isDarkMode ? Brightness.dark : Brightness.light,
+            ),
           ),
-        ),
-        home: WelcomePage(),
-      );
-    });
+          home: WelcomePage(),
+        );
+      },
+    );
   }
 }
-
