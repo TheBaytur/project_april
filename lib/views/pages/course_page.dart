e5d1525a45cecef6a08f24929d568d6dab8e3613
TheBaytur <bayturcodding@gmail.com>
2025-05-22 11:14:33 +0800

    Activity Class F

--- lib/views/pages/course_page.dart
+++ lib/views/pages/course_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:project_april/views/widgets/hero_widget.dart';
+
 
 import '../../data/classes/activity_class.dart';
 
@@ -9,11 +9,12 @@ class CoursePage extends StatefulWidget {
   const CoursePage({super.key});
 
   @override
-  State<CoursePage> createState() => _CoursePageState();
+  State<CoursePage> createState() => CoursePageState();
 }
 
-class _CoursePageState extends State<CoursePage> {
-
+class CoursePageState extends State<CoursePage> {
+  Future <Activity> ? _activityFuture;
+  bool isFirst = true;// <1>
   @override
   void initState() {
     getData();
@@ -21,54 +22,68 @@ class _CoursePageState extends State<CoursePage> {
   }
 
   Future getData() async {
-    var url =
-    Uri.https('bored-api.appbrewery.com', '/random');
+    var url = Uri.https('bored-api.appbrewery.com', '/random');
 
     var response = await http.get(url);
     if (response.statusCode == 200) {
-
-      return Activity.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
-
+      return Activity.fromJson(
+        jsonDecode(response.body) as Map<String, dynamic>,
+      );
     } else {
-
       throw Exception('Failed to load album');
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: FutureBuilder(future: getData(),
-          builder: (context, AsyncSnapshot snapshot) {
-        Widget widget;
+      appBar: AppBar(
+        title: Text('Button'),
+        actions: [
+          IconButton(onPressed: () {
+            setState(() {
+              isFirst = !isFirst;
+            });
 
-        if(snapshot.connectionState == ConnectionState.waiting){
-          return CircularProgressIndicator();
-        }
-        if (snapshot.hasData) {
-          Activity activity = snapshot.data;
-          widget = Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.0),
-            child: SingleChildScrollView(
-              child: Column(children: [
-                HeroWidget(
-                title: activity.activity,
-              ),
-                Text(activity.activity,
+
+
+          }, icon: Icon(Icons.switch_access_shortcut),
+          ),
+        ],
+      ),
+
+      body: FutureBuilder(
+        future: getData(),
+        builder: (context, AsyncSnapshot snapshot) {
+          Widget widget;
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          }
+          if (snapshot.hasData) {
+            Activity activity = snapshot.data;
+            widget = Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: SingleChildScrollView(
+                child: AnimatedCrossFade(
+                  firstChild: Center(
+                    child: Image.asset('assets/images/fridge_logo.png'),
+                  ),
+                  secondChild: Center(
+                    child: Image.asset('assets/images/bg.jpg'),
+                  ),
+                  crossFadeState: isFirst
+                      ? CrossFadeState.showFirst
+                      : CrossFadeState.showSecond,
+                  duration: Duration(milliseconds: 1000),
                 ),
-              ],
               ),
-            ),
-          );
-        } else {
-          widget = Center(child: Text('Error'),
-          );
-        }
-        return widget = widget;
-
-      },
+            );
+          } else {
+            widget = Center(child: Text('Error'));
+          }
+          return widget = widget;
+        },
       ),
     );
   }
